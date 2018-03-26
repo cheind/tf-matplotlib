4134b0f12f708292ea75341abbe30c3b923ab30d
Christoph Heindl <christoph.heindl@gmail.com>
2018-03-26 18:19:20 +0200

    extended tests

--- tfmpl/tests/test_figure.py
+++ tfmpl/tests/test_figure.py
@@ -28,9 +28,39 @@ def test_arguments():
     assert debug['b'] == [0,1]
     np.testing.assert_allclose(debug['c'], np.zeros((2,2)))
     debug['d'] = 'd'
-    debug['e'] = 'e'    
+    debug['e'] = 'e'
 
-    
+
+def test_arguments_blittable():
+
+    debug = {}
+
+    def init(a, b, c, d=None, e=None):
+        debug['init_args'] = [a, b, c, d, e]
+        return tfmpl.create_figure(), None 
+        
+    @tfmpl.blittable_figure_tensor(init_func=init)
+    def draw(a, b, c, d=None, e=None):
+        debug['args'] = [a, b, c, d, e]
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        a = tf.constant(0)
+        c = tf.placeholder(tf.float32)
+
+        tensor = draw(a, [0,1], c, d='d', e='e')
+        sess.run(tensor, feed_dict={c: np.zeros((2,2))})
+
+    assert debug['init_args'][0] == 0
+    assert debug['init_args'][1] == [0,1]
+    np.testing.assert_allclose(debug['init_args'][2], np.zeros((2,2)))
+    assert debug['init_args'][3] == 'd'
+    assert debug['init_args'][4] == 'e'
+
+    assert debug['args'][0] == 0
+    assert debug['args'][1] == [0,1]
+    np.testing.assert_allclose(debug['args'][2], np.zeros((2,2)))
+    assert debug['args'][3] == 'd'
+    assert debug['args'][4] == 'e'
 
 def test_figure():
     assert True == True
