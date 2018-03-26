78bc64dccca5c049edecdc4d042cdc6fca3841b0
Christoph Heindl <christoph.heindl@gmail.com>
2018-03-26 13:51:01 +0200

    adding build support

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tfmpl',
+    version=open('tfmpl/__init__.py').readlines()[-1].split()[-1].strip('\''),
+    description='Seamlessly integrate matplotlib figures tensorflow summaries.',    
+    author='Christoph Heindl',
+    url='https://github.com/cheind/tf-matplotlib',
+    license='MIT',
+    install_requires=required,
+    packages=['tfmpl', 'tfmpl.samples'],
+    include_package_data=True,
+    keywords='tensorflow matplotlib tensorboard'
+)
--- tfmpl/__init__.py
+++ tfmpl/__init__.py
@@ -4,4 +4,3 @@ from tfmpl.create import create_figure, create_figures
 
 # Needs to be last line
 __version__ = '1.0.0'
-
