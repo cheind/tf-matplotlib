e0cb7ffda2f948ff122c2774f2f004adfa240e93
Christoph Heindl <christoph.heindl@gmail.com>
2018-03-26 18:29:22 +0200

    more tests

--- tfmpl/tests/test_figure.py
+++ tfmpl/tests/test_figure.py
@@ -37,7 +37,7 @@ def test_arguments_blittable():
 
     def init(a, b, c, d=None, e=None):
         debug['init_args'] = [a, b, c, d, e]
-        return tfmpl.create_figure(), None 
+        return tfmpl.create_figure(), None
         
     @tfmpl.blittable_figure_tensor(init_func=init)
     def draw(a, b, c, d=None, e=None):
@@ -62,5 +62,56 @@ def test_arguments_blittable():
     assert debug['args'][3] == 'd'
     assert debug['args'][4] == 'e'
 
-def test_figure():
-    assert True == True
+def test_callcount():
+
+    debug = {}
+    debug['called'] = 0
+    debug['a'] = []
+
+    @tfmpl.figure_tensor
+    def draw(a):
+        debug['called'] += 1
+        debug['a'].append(a)        
+        return tfmpl.create_figure()
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        a = tf.placeholder(tf.float32)
+
+        tensor = draw(a)
+
+        for i in range(5):
+            sess.run(tensor, feed_dict={a: i})
+
+    assert debug['called'] == 5
+    np.testing.assert_allclose(debug['a'], [0,1,2,3,4])
+
+def test_callcount_blittable():
+    
+    debug = {}
+    debug['init_called'] = 0
+    debug['draw_called'] = 0
+    debug['a'] = []
+    debug['a_init'] = []
+
+    def init(a):
+        debug['init_called'] += 1
+        debug['a_init'] = a
+        return tfmpl.create_figure(), None
+
+    @tfmpl.blittable_figure_tensor(init_func=init)
+    def draw(a):
+        debug['draw_called'] += 1
+        debug['a'].append(a)        
+        
+    with tf.Session(graph=tf.Graph()) as sess:
+        a = tf.placeholder(tf.float32)
+
+        tensor = draw(a)
+
+        for i in range(5):
+            sess.run(tensor, feed_dict={a: i})
+
+    assert debug['init_called'] == 1
+    assert debug['draw_called'] == 5
+    assert debug['a_init'] == 0
+    np.testing.assert_allclose(debug['a'], [0,1,2,3,4])
